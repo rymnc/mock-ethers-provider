51a443066c66e4fcd7d7d3ff7e5e02555f965117
Aaryamann Challani <aaryamannchallani7@gmail.com>
2020-11-10 21:40:37 +0000

    feat: rework

--- tests/mockedObjects.js
+++ tests/mockedObjects.js
@@ -1,6 +1,6 @@
 const testMap = new Map();
-testMap.set("0x18160ddd", "0x1234");
-testMap.set("0xe5b5019a", "0x721a");
+testMap.set("0x18160ddd", ["0x1234"]);
+testMap.set("0xe5b5019a", ["0x721a"]);
 
 module.exports = {
   testMap,
